66634a7468f5d20e1eacc01dc9aee84c995a6782
Timothy Nguyen <32556246+SirbOss1992@users.noreply.github.com>
2018-05-06 16:03:25 -0700

    Just added invisibility for password field

--- app/src/main/java/cpsc466/ecs/fullerton/edu/cpsc466/MainActivity4_Name_Plan.java
+++ app/src/main/java/cpsc466/ecs/fullerton/edu/cpsc466/MainActivity4_Name_Plan.java
@@ -3,6 +3,7 @@ package cpsc466.ecs.fullerton.edu.cpsc466;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- app/src/main/java/cpsc466/ecs/fullerton/edu/cpsc466/MainActivity5_Choose_Destinations.java
+++ app/src/main/java/cpsc466/ecs/fullerton/edu/cpsc466/MainActivity5_Choose_Destinations.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -54,6 +55,7 @@ public class MainActivity5_Choose_Destinations extends AppCompatActivity {
         planName = getName.getStringExtra("Name");
         planDate = getDate.getStringExtra("Date");
         username = getUsername.getStringExtra("Username");
+
 
         robot4Less = (CheckBox) findViewById(R.id.robot4Less);
         westCoastRecyclingCenter = (CheckBox) findViewById(R.id.westCoastRecyclingCenter);
